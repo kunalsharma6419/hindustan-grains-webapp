7d841755311465b3cb592697ea05d728c511d2d4
prajapatineeraj204-dev <prajapatineeraj204@gmail.com>
2024-03-12 23:45:15 +0530

    admin dashboard update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,41 @@
 
 @section('content')
 <div class="row">
-  </div>  
+  <div class="col-md-3  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Promoter</h2>
+      <h5 class="text-center">{{$user}}</h5>
+    </div>
+  </div>
+<div class="col-md-3  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Product</h2>
+      <h5 class="text-center">{{$product}}</h5>
+    </div>
+  </div>
+  <div class="col-md-3  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Customer</h2>
+      <h5 class="text-center">{{$customer}}</h5>
+    </div>
+  </div>
+  <div class="col-md-3  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Product Invoice</h2>
+      <h5 class="text-center">{{$ProductInvoice}}</h5>
+    </div>
+  </div>
+  <div class="col-md-4  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Total Grant</h2>
+      <h5 class="text-center">Rs{{$producttotalgrant}}</h5>
+    </div>
+  </div>
+  <div class="col-md-6  mt-3 card text-center">
+    <div class="card-body">
+      <h2>Total Receive Payment</h2>
+      <h5 class="text-center">Rs{{$paymentstatus}}</h5>
+    </div>
+  </div>
+</div>  
 @endsection
