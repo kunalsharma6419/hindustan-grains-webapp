a4637e4b5541287175153e3aa94605bd0c36353f
kunalsharma6419 <kunal4164@gmail.com>
2024-04-16 19:31:21 +0530

    pwa login signup handle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hindustan Grains Login</title>
+    <title>Hindustan Grains</title>
+    {{ pwa_meta() }}
 </head>
 <script src="https://cdn.tailwindcss.com"></script>
 <script>
